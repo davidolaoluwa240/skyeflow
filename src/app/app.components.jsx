b72fb47c56b4d0e8a6984ff8721bc5dd4eca26e7
davidolaoluwa240 <davidolaoluwa240@gmail.com>
2022-11-19 21:07:14 +0200

    basic setup and started dashboard layout

---
+++ src/app/app.components.jsx
@@ -0,0 +1,20 @@
+// Modules
+import React from "react";
+
+// Components
+import { Routes, Route } from "react-router-dom";
+
+// Style
+import "./app.styles.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/layouts/dahboardLayout/dashboardLayout.component.jsx
@@ -0,0 +1,12 @@
+// Modules
+import React from "react";
+
+const DashboardLayout = ({ className }) => {
+  return <div className="dashboard">dashboardLayout.component</div>;
+};
+
+DashboardLayout.defaultProps = {
+  className: "",
+};
+
+export default DashboardLayout;
